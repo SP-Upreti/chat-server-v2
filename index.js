ea5f8a6e10164324c785ab35e5f898c4b2e4e200
SP-Upreti <surendraupreti441@gmail.com>
2024-12-21 19:56:11 +0545

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors({
 
 // Add headers explicitly for all routes (optional)
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', ['http://localhost:3000', 'https://chattter-gilt.vercel.app']);
+    res.header('Access-Control-Allow-Origin', 'https://chattter-gilt.vercel.app');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
